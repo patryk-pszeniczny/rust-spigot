17d07304193f7872285a5e67c990bc33d9a57c1c
Patryk Pszeniczny <vcode1@onet.pl>
2025-10-30 11:22:05 +0100

    Day 13 of the Rust leaks (Oct 25, 2025).

---
+++ src/main/java/uniquecode/rust/disco/enums/ArmorType.java
@@ -0,0 +1,13 @@
+package uniquecode.rust.disco.enums;
+
+public enum ArmorType {
+    SMOOTH("SMOOTH", 0),
+    RANDOM("RANDOM", 1),
+    GRAY("GRAY", 2),
+    POLICE("POLICE", 3),
+    FADE("FADE", 4),
+    VERDUN("VERDUN", 5),
+    SZYMON("SZYMON", 6);
+    ArmorType(String s, int n) {
+    }
+}
